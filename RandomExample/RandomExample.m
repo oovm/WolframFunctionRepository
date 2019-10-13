b37bd2d40681d08dc928568374e2a717ec25d0ad
Aster <galaster@foxmail.com>
2019-10-14 02:57:07 +0800

    🎬 New function: RandomExample

---
+++ RandomExample/RandomExample.m
@@ -0,0 +1,40 @@
+(* ::Package:: *)
+
+(* ::Section:: *)
+(*Definition*)
+
+
+(* ::Subsubsection:: *)
+(*Wrap Function*)
+
+Options[RandomExample] = {};
+RandomExample[o : OptionsPattern[]] := Block[
+	{title, text, file, inputs, output, cap, in, i = 0, j = 1},
+	file = RandomChoice[$ExampleNotebooks];
+	inputs = Import[file, {"Cells", "Input"}];
+	output = Import[file, {"Cells", "Output"}][[1]];
+	cap = CellLabel /. Options[output];
+	If[!StringQ[cap], Return[$Failed]];
+	cap = ToExpression[StringReplace[cap, "Out[" ~~ x__ ~~ "]" ~~ __ :> x]];
+	title = Cell[StringReplace[file, __ ~~ "ExamplePages" :> "ExamplePages"], "Subsubsection"];
+	text = Reap@While[
+		i < cap && j <= Length[inputs],
+		in = CellLabel /. Options[inputs[[j]]];
+		If[StringQ[in], i = ToExpression[StringReplace[in, "In[" ~~ x__ ~~ "]" ~~ __ :> x]]];
+		Sow[inputs[[j++]]]
+	]    ;
+	Paste@Cell[
+		CellGroup[Flatten@{title, text[[-1, 1]], output}, CellGrouping -> Automatic],
+		"Output",
+		CellAutoOverwrite -> True,
+		GeneratedCell -> True
+	]
+];
+
+
+(* ::Subsubsection:: *)
+(*Main Function*)
+
+
+$ExampleNotebooks := $ExampleNotebooks = FileNames["*", DirectoryName[FindFile["ExamplePages/3DLaplacians.nb"]]];
+
--- Readme.md
+++ Readme.md
@@ -2,3 +2,20 @@ Function Issue Tracer
 =====================
 
 Tracking the features and issues of these functions I uploaded.
+
+
+
+
+### FastMod
+
+- [BinomialMod](./BinomialMod)
+
+### Painting
+
+- [LineWebPainting](./LineWebPainting)
+- [TrianglePainting](./TrianglePainting)
+- [PolygonPainting](./PolygonPainting)
+
+### Random
+
+- [RandomMondrian](./RandomMondrian)
