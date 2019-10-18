5f41cef221254cbd5891c448ec84a6e3588c72ec
Aster <galaster@foxmail.com>
2019-10-19 00:14:57 +0800

    🌿 Add rule to export code

--- NotebookToMarkdown/NotebookToMarkdown.m
+++ NotebookToMarkdown/NotebookToMarkdown.m
@@ -12,10 +12,62 @@ NotebookToMarkdown[s_, o___] := (
 );
 
 
-NotebookToMarkdown["Title", t_, co_CellObject] := TemplateApply["# ``\n\n", {t}]
-NotebookToMarkdown["Subtitle", t_, co_CellObject] := TemplateApply["## ``\n\n", {t}]
-NotebookToMarkdown["Chapter", t_, co_CellObject] := TemplateApply["### ``\n\n", {t}]
-NotebookToMarkdown["Section", t_, co_CellObject] := TemplateApply["#### ``\n\n", {t}]
-NotebookToMarkdown["Subsection", t_, co_CellObject] := TemplateApply["##### ``\n\n", {t}]
-NotebookToMarkdown["Subsubsection", t_, co_CellObject] := TemplateApply["###### ``\n\n", {t}]
+NotebookToMarkdown["Title", data_, co_CellObject] := {"#", parseData@data, "\n\n"};
+NotebookToMarkdown["Subtitle", data_, co_CellObject] := {"##", parseData@data, "\n\n"};
+NotebookToMarkdown["Chapter", data_, co_CellObject] := {"###", parseData@data, "\n\n"};
+NotebookToMarkdown["Section", data_, co_CellObject] := {"####", parseData@data, "\n\n"};
+NotebookToMarkdown["Subsection", data_, co_CellObject] := {"#####", parseData@data, "\n\n"};
+NotebookToMarkdown["Subsubsection", data_, co_CellObject] := {"######", parseData@data, "\n\n"};
 
+
+NotebookToMarkdown["Text", data_, co_CellObject] := {parseData@data, "\n\n"};
+
+
+codeStyleQ = MemberQ[{"Code", "Input"}, #] &;
+parseCodeData[data_, indent_ : 4] := Block[
+	{s = ConstantArray[" ", indent], fe},
+	fe = FrontEndExecute[FrontEnd`ExportPacket[data, "PlainText"]];
+	s <> StringReplace[First@fe , "\r\n" | "\n" -> "\n" <> s]
+];
+NotebookToMarkdown[style_?codeStyleQ, data_, cellObj_CellObject] := {
+	"```mathematica\n",
+	parseCodeData[data, 0],
+	"\n```\n\n"
+};
+
+
+parseData[list_List] := parseData /@ list;
+parseData[string_String] := string;
+parseData[data_(BoxData | TextData)] := List @@ (parseData /@ data);
+parseData[cell_Cell] := parseData@First@cell;
+parseData[boxes_] := (
+	Echo[Inactive[parseData][boxes], "Todo: "];
+	parseData@First@boxes
+);
+
+(*
+parseData[StyleBox[expr_, opts___]] := styleWrapper[opts]@parseData[expr];
+styleWrapper[opts___] := Module[
+	{italic, bold, wrapper },
+	italic = MemberQ[{opts}, Verbatim[Rule][FontSlant, "Italic"]];
+	bold = MemberQ[{opts}, Verbatim[Rule][FontWeight, "Bold"]];
+	wrapper = Which[
+		bold, "**",
+		italic, "*",
+		True, ""
+	];
+	wrapper <> # <> wrapper &
+];
+
+
+parseData[FormBox[boxes : Except[_TagBox], TraditionalForm, ___]] := Module[{teXForm}
+	, teXForm = boxesToTeX@boxes
+	; "$" <> teXForm <> "$"
+];
+boxesToTeX = ToString[ToExpression@#, TeXForm] &;
+
+parseData[box : ButtonBox[_, ___, BaseStyle -> "Hyperlink", ___]] := Module[{label, url}
+	, {label, url} = {#, #2} & @@ ToExpression[box]
+	;  TemplateApply["[``](``)", {StringJoin@Flatten@{parseData@label}, url}]
+];
+*)
