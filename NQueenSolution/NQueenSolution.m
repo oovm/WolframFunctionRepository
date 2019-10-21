8fea70ee8ac6d4cb18a6ff2586ee75013af6e080
Aster <galaster@foxmail.com>
2019-10-22 00:52:43 +0800

    🌿 Add iteration solve method

--- NQueenSolution/NQueenSolution.m
+++ NQueenSolution/NQueenSolution.m
@@ -8,9 +8,21 @@
 (*Wrap Function*)
 
 
+Options[NQueenSolution] = {};
+NQueenSolution[n_Integer] := Block[
+	{},
+	LinearProgrammingSolver[n]
+];
+
+
 (* ::Subsubsection:: *)
 (*Main Function*)
 
+
+IterationSolverAll[n_] := With[
+	{step = Join @@ Table[Append[a, b], {a, #}, {b, With[{t = Range[Length@a, 1, -1]}, Complement[Range[n], a - t, a, a + t]]}]&},
+	Nest[step, {{}}, n]
+];
 
 LinearProgrammingSolver[n_] := Module[
 	{c, m, b, vars},
@@ -18,9 +30,8 @@ LinearProgrammingSolver[n_] := Module[
 	m = mQueens[n];
 	vars = mQueens2[n];
 	b = bQueens[Length[m]];
-	Partition[LinearProgramming[c, m, b, vars, Integers], n]
+	1 + Mod[Flatten@Position[LinearProgramming[c, m, b, vars, Integers], 1], n]
 ];
-
 
 
 mQueens[n_] := Module[
@@ -35,37 +46,43 @@ mQueens2[n_] := Table[{0, 1}, {i, n^2}];
 cQueens[n_] := Table[-1, {i, n^2}];
 bQueens[l_] := Table[{1, -1}, {i, l}];
 
-mQueensH[n_] := Module[
+mQueensH[n_] := Block[
 	{t},
 	t = Table[0, {i, n}, {j, n^2}];
-	For[i = 1, i <= n, i++,
-		For[j = 1, j <= n, j++, t[[i, ((i - 1) * n) + j]] = 1]]; t
+	For[i = 1, i <= n, i++, For[j = 1, j <= n, j++, t[[i, ((i - 1) * n) + j]] = 1]];
+	Return@t
 ];
-
-mQueensV[n_] := Module[
+mQueensV[n_] := Block[
 	{t},
 	t = Table[0, {i, n}, {j, n^2}];
-	For[i = 1, i <= n, i++,
-		For[j = 1, j <= n, j++, t[[j, ((i - 1) * n) + j]] = 1]]; t
+	For[i = 1, i <= n, i++, For[j = 1, j <= n, j++, t[[j, ((i - 1) * n) + j]] = 1]];
+	Return@t
 ];
-
-mQueensD[n_] := Module[
+mQueensD[n_] := Block[
 	{t},
 	t = Table[0, {i, (2 * n) - 1}, {j, n^2}];
 	For[k = 2, k <= 2 n, k++,
 		For[i = 1, i <= n, i++,
 			For[j = 1, j <= n, j++,
-				If[i + j == k, t[[k - 1, ((i - 1) * n) + j]] = 1]]]]; t
+				If[i + j == k, t[[k - 1, ((i - 1) * n) + j]] = 1]
+			]
+		]
+	];
+	Return@t
 ];
-
-mQueensDM[n_] := Module[
+mQueensDM[n_] := Block[
 	{t},
 	t = Table[0, {i, Sum[1, {i, 1 - n, n - 1}]}, {j, n^2}];
 	For[k = 1 - n, k <= n - 1, k++,
 		For[i = 1, i <= n, i++,
 			For[j = 1, j <= n, j++,
-				If[i == j - k, t[[k + n, ((i - 1) * n) + j]] = 1]]]]; t
+				If[i == j - k, t[[k + n, ((i - 1) * n) + j]] = 1]
+			]
+		]
+	];
+	Return@t
 ];
 
 
-LinearProgrammingSolver[100]; // AbsoluteTiming
+LinearProgrammingSolver[4] // AbsoluteTiming
+
--- NQueenSolution/Readme.md
+++ NQueenSolution/Readme.md
@@ -1,4 +1,51 @@
-# Template
+# NQueenSolution
+
+
+## Reference
+
+- https://rosettacode.org/wiki/N-queens_problem#Mathematica
+
+- https://mathematica.stackexchange.com/questions/164930
+
+
+
+
+```mathematica
+Count[Permutations[v=Range@8],And@@Unequal@@@{#+v,#-v}&]
+```
+
+Only 57 characters to give all solutions!
+
+```mathematica
+n = 16;
+f = With[
+	{X = Symbol["x" <> ToString[#]]&},
+	Table[
+		{
+			X[j + 1],
+			If[
+				Or @@ Table[X[j] == X[j - i] || Abs[X[j] - X[j - i]] == i, {i, j - 1}] /. f : _Or :> Sort[f],
+				0,
+				Evaluate@If[j < n, n, 1]
+			]
+		},
+		{j, n}
+	]
+];
+AbsoluteTiming[cf = f /. {iter__} :> Compile[
+	{{x1, _Integer}},
+	Module[
+		{cnt = 0},
+		Do[cnt++, iter];cnt
+	],
+	CompilationTarget -> "C",
+	RuntimeOptions -> "Speed",
+	RuntimeAttributes -> {Listable}
+]];
+Total@cf@Range[n] // AbsoluteTiming
+2Total@Most[#] + If[OddQ@n, Last@#, 0]&@cf@Range[n / 2 + 1] // AbsoluteTiming
+```
+
 
 ## Changelog
 
