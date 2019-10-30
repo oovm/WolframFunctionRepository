1f019f56c96e355b77bb02ca194671458b35e9de
Aster <galaster@foxmail.com>
2019-10-30 09:42:52 +0800

    🌿 Fix problem of lost state when exit session

--- LightsOutGame/LightsOutGame.m
+++ LightsOutGame/LightsOutGame.m
@@ -4,45 +4,67 @@
 (*Definition*)
 
 
-CC
-
-
 (* ::Subsubsection:: *)
 (*Wrap Function*)
+
+
+Options[LightsOutGame] = {
+	Mode -> Random,
+	ImageSize -> {400, 400}
+};
+LightsOutGame[n_Integer, o : OptionsPattern[]] := LightsOutGame[n, n, o];
+LightsOutGame[m_Integer, n_Integer, o : OptionsPattern[]] := Block[
+	{game},
+	If[m < 1 || n < 1, Return[]];
+	game = Switch[
+		OptionValue[Mode],
+		Random, RandomInteger[1, {m, n}],
+		Normal, LightsNormal[m, n],
+		_, Return[]
+	];
+	LightsOutGame[game, o]
+];
+LightsOutGame[M_List, o : OptionsPattern[]] := gamePlay[M, o];
 
 
 (* ::Subsubsection:: *)
 (*Main Function*)
 
 
-
-
-
-gs = {{0, 0}, {0, 0}}
-Options[gamePlay] = {
-	ImageSize -> {400, 400}
-
-};
-gamePlay[gameSquare_, o : OptionsPattern[]] := Module[
+Options[gamePlay] = Options[LightsOutGame];
+gamePlay[inSquare_, o : OptionsPattern[]] := DynamicModule[
 	{
-		$start = Now,
+
 		$win = False,
+		$start = Now,
+		$finish = 0,
 		$step = 0,
-		gs = gameSquare,
-		w, h
+		$final = 0,
+		truth = Transpose@inSquare,
+		w, h, state, updateState
 	},
-	w = Length[First@gs];
-	h = Length[gs];
+	{w, h} = Length /@ {truth, First@truth};
+	state[] := Table[EvenQ[getShowValue[i, j, w, h, truth]], {i, 1, w}, {j, 1, h}];
+	updateState[{i_, j_}] := (
+		truth[[i, j]] = 1 - truth[[i, j]];
+		If[
+			AllTrue[Flatten@state[], TrueQ] && !TrueQ@$win,
+			$win = True;
+			$final = $step + 1 ;
+			$finish = Now - $start
+		];
+		$step += 1;
+	);
 	EventHandler[
 		Panel@Column[{
 			"The Lights Out Game",
-			Dynamic[Row[{Style["Steps: ", Bold], If[$win, Null, $step, $step], If[$win, Nothing, Nothing, Style["  You Win!", Red]]}]],
-			Dynamic[Row[{Style["Time: ", Bold], If[$win, Null, Now - $start, $win]}], UpdateInterval -> 1 / 24],
+			Dynamic[Row[{Style["Steps: ", Bold], If[$win, $final, $step], If[$win, Style["  You Win!", Red], Nothing ]}]],
+			Dynamic[Row[{Style["Time: ", Bold], If[$win, $finish, Now - $start]}], UpdateInterval -> 1 / 24],
 			Graphics[
 				Dynamic[
 					Table[
 						If[
-							EvenQ[getShowValue[i, j, w, h, gs]],
+							EvenQ[getShowValue[i, j, w, h, truth]],
 							{Black, showSquare[{i, j}]},
 							{White, showSquare[{i, j}]}
 						],
@@ -55,25 +77,19 @@ gamePlay[gameSquare_, o : OptionsPattern[]] := Module[
 			]
 		}],
 		"MouseDown" :> updateState[
-			Flatten@{Floor[MousePosition["Graphics"]], $start},
-			gs,
-			$step,
-			$win
+			Floor[MousePosition["Graphics"]]
 		]
-	]
-]
-
-
-
-SetAttributes[updateState, HoldRest];
-updateState[{i_, j_, start_}, gs_, n_, win_] := Block[
-	{},
-	gs[[i, j]] = 1 - gs[[i, j]];
-	If[Total[1 - gs, 2] == 0, win = Now - start];
-	n += 1;
+	],
+	SaveDefinitions -> True
 ];
-showSquare[{i_, j_}] := Rectangle[{i + 0.02, j + 0.02}, {i + 1 - 0.02, j + 1 - 0.02}];
 
+
+
+(* ::Subsubsection:: *)
+(*Auxiliary Functions*)
+
+
+showSquare[{i_, j_}] := Rectangle[{i + 0.02, j + 0.02}, {i + 1 - 0.02, j + 1 - 0.02}];
 getSquareValue[i_, j_, w_, h_, gs_] := If[i >= 1 && i <= w && j >= 1 && j <= h, gs[[i, j]], 0];
 getShowValue[i_, j_, w_, h_, gs_] := Plus[
 	getSquareValue[i, j, w, h, gs],
@@ -81,4 +97,10 @@ getShowValue[i_, j_, w_, h_, gs_] := Plus[
 	getSquareValue[i, j - 1, w, h, gs],
 	getSquareValue[i - 1, j, w, h, gs],
 	getSquareValue[i + 1, j, w, h, gs]
+];
+
+
+LightsNormal[m_, n_] := Module[
+	{mat = AdjacencyMatrix[GridGraph[{n, m}]] + IdentityMatrix[n * m]},
+	Partition[LinearSolve[mat, Table[1, {n * m}], Modulus -> 2], n]
 ];
--- LightsOutGame/Readme.md
+++ LightsOutGame/Readme.md
@@ -1,4 +1,10 @@
 # Template
 
+
+http://mathworld.wolfram.com/LightsOutPuzzle.html
+
 ## Changelog
 
+v1.2 
+
+Fix LightsOutGame's problem of lost state when exit session
