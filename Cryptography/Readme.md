5d21c3bbc7b68279ad980c0fbf7b0b411aa713f6
Aster <galaster@foxmail.com>
2019-11-04 22:15:44 +0800

    🌿 Fixed an issue with a wrong length of time

---
+++ Cryptography/Readme.md
@@ -0,0 +1,12 @@
+# Template
+
+
+https://zhuanlan.zhihu.com/p/80587769
+
+https://www.zhihu.com/question/22506052
+
+## Changelog
+
+### v1.1 
+
+修复了 `MorseCipher` 中 unicode 编码和原始 morse 编码重复的问题
--- Cryptography/SteganographyAudio.m
+++ Cryptography/SteganographyAudio.m
@@ -13,17 +13,22 @@
 
 
 Options[SteganographyAudio] = {
-	"Time" -> 10,
-	Rescale -> 60(*Hidden*)
+	Duration -> 10
 };
 SteganographyAudio[s_String, o : OptionsPattern[]] := SteganographyAudio[s, 2000, o];
 SteganographyAudio[s_String, sample_Integer, o : OptionsPattern[]] := Block[
-	{time, r, text, imgdata, list, listcompete},
-	{time, r} = OptionValue[{"Time", Rescale}];
+	{time, text, imgdata, list, listcompete},
+	time = OptionValue[Duration];
+	time = Switch[
+		Head@time,
+		Quantity, First@UnitConvert[time, "Seconds"],
+		Integer, time,
+		_, Return[]
+	];
 	text = Text@TemplateApply[" `1` ", {s}];
 	imgdata = Reverse@ImageData[ColorNegate@Binarize[ImageResize[Rasterize[text, RasterSize -> 100], {100, Automatic}]]];
 	list = Flatten[Position[#, 1]]& /@ Transpose[imgdata];
-	listcompete = Flatten[Table[Table[Total[Sin[2.0 Pi sample / r # t]&@(#& /@ list)[[k]]], {t, 0, time / 100, 1 / sample}], {k, 1, Length@list}], 1];
+	listcompete = Flatten[Table[Table[Total[Sin[2.0 Pi sample / 60 # t]&@(#& /@ list)[[k]]], {t, 0, time / 100, 1 / sample}], {k, 1, Length@list}], 1];
 	Audio[Sound[SampledSoundList[listcompete, sample]]]
 ];
 
