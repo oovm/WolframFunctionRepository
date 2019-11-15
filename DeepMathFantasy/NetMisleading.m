67738ab4b5ed9b6cc647487fd60f95f1bc53ce18
Aster <galaster@foxmail.com>
2019-11-16 03:23:32 +0800

    🌿 Add ImageClassifyQ

--- DeepMathFantasy/NetMisleading.m
+++ DeepMathFantasy/NetMisleading.m
@@ -10,11 +10,21 @@
 
 (* ::Subsubsection:: *)
 (*Main Functions*)
-NetMisleading[net_, img_Image, o : OptionsPattern[]] := NetMisleading[net, img, Automatic, o]
-NetMisleading[net_?ImageClassifyQ, i_Image, target_, o : OptionsPattern[]] := Block[
-	{size, img, forward, trainedNet},
-	size = NetExtract[net, {"Input", "ImageSize"}];
-	img = ImageResize[RemoveAlphaChannel@ColorConvert[i, "RGB"], size];
+NetMisleading[net_, img_, o : OptionsPattern[]] := NetMisleading[net, img, Automatic, o];
+NetMisleading[net_?ImageClassifyQ, i_Image, task_, o : OptionsPattern[]] := Block[
+	{encoder, decoder, target, img, forward, trainedNet},
+	encoder = NetExtract[net, "Input"];
+	decoder = NetExtract[net, "Output"];
+	target = If[
+		task === Automatic,
+		RandomChoice@decoder[["Labels"]],
+		If[
+			!MemberQ[decoder[["Labels"]], task],
+			Return[Missing["NoSuchLabel"]],
+			task
+		]
+	];
+	img = ImageResize[RemoveAlphaChannel@ColorConvert[i, "RGB"], encoder[["ImageSize"]]];
 	forward = NetChain[
 		<|
 			"image" -> ConstantArrayLayer["Array" -> ImageData[img, Interleaving -> False]],
@@ -25,13 +35,24 @@ NetMisleading[net_?ImageClassifyQ, i_Image, target_, o : OptionsPattern[]] := Bl
 	];
 	trainedNet = NetTrain[
 		forward,
-		<|"Output" -> {task}|>,
+		<|"Output" -> {target}|>,
 		LearningRateMultipliers -> {"image" -> 1, _ -> None},
 		MaxTrainingRounds -> 64,
 		TargetDevice -> "GPU"
 	];
 	Image[NetExtract[trainedNet, {"image", "Array"}], Interleaving -> False]
-]
+];
 
 (* ::Subsubsection:: *)
 (*Auxiliary Functions*)
+ImageClassifyQ[net_] := Block[
+	{encoder, decoder, type},
+	If[!NeuralNetworks`ValidNetQ@net, Return[False]];
+	encoder = NetExtract[net, "Input"];
+	type = encoder /. HoldPattern[NetEncoder[kind_, __]] :> kind ;
+	If[type != "Image", Return[False]];
+	decoder = NetExtract[net, "Output"];
+	type = encoder /. HoldPattern[NetEncoder[kind_, __]] :> kind ;
+	If[type != "Class", Return[False]];
+	Return[True]
+];
