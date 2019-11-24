c352bca4bfe47696c1823cade4cf3675272c8a8e
Aster <galaster@foxmail.com>
2019-11-24 18:23:55 +0800

    🌿 Make color box for RYBColor

--- RYBColor/RYBColor.m
+++ RYBColor/RYBColor.m
@@ -14,3 +14,21 @@
 
 (* ::Subsubsection:: *)
 (*Auxiliary Functions*)
+TooltipBox[
+	GraphicsBox[
+		{
+			{GrayLevel[0], RectangleBox[{0, 0}]},
+			{GrayLevel[0], RectangleBox[{1, -1}]},
+			{RGBColor[0.5`, 1, 0.5`, 1], RectangleBox[{0, -1}, {2, 1}]}
+		},
+		DefaultBaseStyle -> "ColorSwatchGraphics",
+		AspectRatio -> 1,
+		Frame -> True,
+		FrameStyle -> Darker[RGBColor[0.5`, 1, 0.5`, 1]],
+		FrameTicks -> None,
+		PlotRangePadding -> None,
+		ImageSize -> {Automatic, 10.8}
+	],
+	StyleBox[RowBox[{"RYBColor[", "0.5`", ",", "1", ",", "0.5`", ",", "1", "]"}], NumberMarks -> False]
+]
+ToExpression[%, StandardForm]
