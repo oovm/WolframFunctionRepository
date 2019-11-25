fc93d73400cdd1333772b9eca7ad5a8b2410b627
Aster <galaster@foxmail.com>
2019-11-26 00:53:44 +0800

    🌿 Better cache manager

--- PhotoAssemble/PhotoAssemble.m
+++ PhotoAssemble/PhotoAssemble.m
@@ -6,7 +6,7 @@
 
 (* ::Subsection:: *)
 (*Wrap Function*)
-
+$ImageBlockSize = 50;
 
 (* ::Subsection:: *)
 (*Main Functions*)
@@ -15,21 +15,33 @@
 (* ::Subsection:: *)
 (*Auxiliary Functions*)
 
-getCache[path_String] := Block[
+getCache[l_List, max_] := Block[
+	{images, cache},
+	images = Take[l, UpTo[max]];
+	Echo[Row[{Text["cache save to  "], Style[".mosaic-cache.mat", Bold]}], "Cache: "];
+	cache = Monitor[
+		Table[getThumbnail[images[[i]]], {i, Length@images}],
+		ProgressIndicator[i, {1, Length@images}]
+	];
+	Export[".mosaic-cache.mat", cache, "WXF"];
+	Return@cache
+];
+getCache[path_String, max_] := Block[
 	{temp, cache, files},
 	If[!DirectoryQ@path, Return@loadCache[path]];
-	temp = FileNameJoin[{dir, "mosaic.cache.mat"}];
+	temp = FileNameJoin[{path, ".mosaic-cache.mat"}];
 	If[FileExistsQ@temp, Return@loadCache[temp]];
-	files = FileNames[{"*.jpg", "*.jpeg", "*.png", "*.tiff"}, dir, Infinity];
-	cache = getThumbnail /@ files;
-	saveCache[temp, cache];
+	Echo[Row[{Text["cache save to  "], Style[temp, Bold]}], "Cache: "];
+	files = FileNames[{"*.jpg", "*.jpeg", "*.png", "*.tiff"}, path, Infinity];
+	files = Take[files, UpTo[max]];
+	cache = Monitor[
+		Table[getThumbnail[files[[i]]], {i, Length@files}],
+		ProgressIndicator[i, {1, Length@files}]
+	];
+	Export[temp, cache, "WXF"];
 	Return@cache
-]
+];
 loadCache[file_String] := (
 	Echo[Row[{Text["cache load from  "], Style[file, Bold]}], "Cache: "];
 	Check[Import[file, "WXF"], {}]
-);
-saveCache[path_String, o_] := (
-	Echo[Row[{Text["cache save to  "], Style[path, Bold]}], "Cache: "];
-	Export[path, o, "WXF"]
-);
+)
