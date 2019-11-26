a086b6a912666caea8bfbbd021dd4c1a8744eb67
Aster <galaster@foxmail.com>
2019-11-27 00:16:18 +0800

    🌿 TupperK encryption algorithm

--- Cryptography/TupperCipher.m
+++ Cryptography/TupperCipher.m
@@ -4,9 +4,21 @@
 (*Definition*)
 
 
-(* ::Subsubsection:: *)
+(* ::Subsection:: *)
 (*Wrap Function*)
 
 
-(* ::Subsubsection:: *)
-(*Main Function*)
+(* ::Subsection:: *)
+(*Main Functions*)
+Options[TupperK] = {Prime -> 17, Times -> 6};
+TupperK[img_, OptionsPattern[]] := Module[
+	{i, m, r, p},
+	p = OptionValue[Prime];
+	i = Rasterize[img, ImageSize -> {OptionValue[Times] p, p}];
+	m = Map[Boole[Max@# < 1] &, ImageData[i], {2}];
+	r = p FromDigits[Flatten[Reverse@Transpose[m]], 2]
+]
+
+(* ::Subsection:: *)
+(*Auxiliary Functions*)
+
