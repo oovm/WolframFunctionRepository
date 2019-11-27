02e3f5d837138d8f48be4347e6be322f0c7dc511
Aster <galaster@foxmail.com>
2019-11-28 06:13:45 +0800

    🌿 TupperPlot basic options

--- Cryptography/TupperDecipher.m
+++ Cryptography/TupperDecipher.m
@@ -4,9 +4,21 @@
 (*Definition*)
 
 
-(* ::Subsubsection:: *)
+(* ::Subsection:: *)
 (*Wrap Function*)
 
 
-(* ::Subsubsection:: *)
-(*Main Function*)
+(* ::Subsection:: *)
+(*Main Functions*)
+Options[TupperPlot] = {Prime -> 17, Times -> 6};
+TupperPlot[n_, OptionsPattern[]] := Module[
+	{bin, p, t},
+	p = OptionValue[Prime]; t = OptionValue[Times] ;
+	bin = IntegerDigits[n / p, 2, t p^2 - 1];
+	ArrayPlot[Reverse /@ Transpose@Partition[bin, p]]
+]
+
+(* ::Subsection:: *)
+(*Auxiliary Functions*)
+
+
