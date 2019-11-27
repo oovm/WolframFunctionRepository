cf6d81eed389a599bc9db31c93dd799f293af14c
Aster <galaster@foxmail.com>
2019-11-28 06:14:57 +0800

    🌿 TupperPlot use shift to control plot range

--- Cryptography/TupperDecipher.m
+++ Cryptography/TupperDecipher.m
@@ -10,11 +10,15 @@
 
 (* ::Subsection:: *)
 (*Main Functions*)
-Options[TupperPlot] = {Prime -> 17, Times -> 6};
-TupperPlot[n_, OptionsPattern[]] := Module[
-	{bin, p, t},
-	p = OptionValue[Prime]; t = OptionValue[Times] ;
-	bin = IntegerDigits[n / p, 2, t p^2 - 1];
+Options[TupperPlot] = {
+	"Shift" -> 6
+};
+TupperPlot[n_, o : OptionsPattern[]] := TupperPlot[n, 17, o];
+TupperPlot[n_, p_?PrimeQ, o : OptionsPattern[]] := Module[
+	{bin, t},
+	t = Max[Ceiling@OptionValue["Shift"], 1];
+	Print[Length@IntegerDigits[n / p, 2]];
+	bin = IntegerDigits[n / p, 2, p * Ceiling[IntegerLength[n / p, 2] / p]];
 	ArrayPlot[Reverse /@ Transpose@Partition[bin, p]]
 ]
 
