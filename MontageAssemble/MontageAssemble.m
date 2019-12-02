9da764a16573eea3c5683a41854c0a26d816c14b
Aster <galaster@foxmail.com>
2019-12-02 10:52:20 +0800

    🐣 Rename as MontageAssemble

--- MontageAssemble/MontageAssemble.m
+++ MontageAssemble/MontageAssemble.m
@@ -6,11 +6,12 @@
 
 (* ::Subsection:: *)
 (*Wrap Function*)
-$ImageBlockSize = 50;
+
 
 (* ::Subsection:: *)
 (*Main Functions*)
-
+$ImageBlockSize = 50;
+$CacheFileName = ".mosaic-cache.mat";
 
 (* ::Subsection:: *)
 (*Auxiliary Functions*)
@@ -18,18 +19,18 @@ $ImageBlockSize = 50;
 getCache[l_List, max_] := Block[
 	{images, cache},
 	images = Take[l, UpTo[max]];
-	Echo[Row[{Text["cache save to  "], Style[".mosaic-cache.mat", Bold]}], "Cache: "];
+	Echo[Row[{Text["cache save to  "], Style[$CacheFileName, Bold]}], "Cache: "];
 	cache = Monitor[
 		Table[getThumbnail[images[[i]]], {i, Length@images}],
 		ProgressIndicator[i, {1, Length@images}]
 	];
-	Export[".mosaic-cache.mat", cache, "WXF"];
+	Export[$CacheFileName, cache, "WXF"];
 	Return@cache
 ];
 getCache[path_String, max_] := Block[
 	{temp, cache, files},
 	If[!DirectoryQ@path, Return@loadCache[path]];
-	temp = FileNameJoin[{path, ".mosaic-cache.mat"}];
+	temp = FileNameJoin[{path, $CacheFileName}];
 	If[FileExistsQ@temp, Return@loadCache[temp]];
 	Echo[Row[{Text["cache save to  "], Style[temp, Bold]}], "Cache: "];
 	files = FileNames[{"*.jpg", "*.jpeg", "*.png", "*.tiff"}, path, Infinity];
@@ -44,4 +45,17 @@ getCache[path_String, max_] := Block[
 loadCache[file_String] := (
 	Echo[Row[{Text["cache load from  "], Style[file, Bold]}], "Cache: "];
 	Check[Import[file, "WXF"], {}]
-)
+);
+getThumbnail[str_String] := getThumbnail[File@str];
+getThumbnail[file_File] := With[
+	{i = Import[file]},
+	getThumbnail[i]
+];
+getThumbnail[img_Image] := Block[
+	{i = ImageResize[RemoveAlphaChannel@img, {$ImageBlockSize, $ImageBlockSize}]},
+	{
+		i,
+		N@Mean[Flatten[ImageData[i], 1]],
+		N@Mean[Flatten@ImageData@ColorConvert[i, "Grayscale"]]
+	}
+];
