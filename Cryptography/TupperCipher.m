0a800a4447246576ac016e00b5969ee557867afd
Aster <galaster@foxmail.com>
2019-12-04 02:59:48 +0800

    🌿 Add more options for Tupper Cipher and Decipher

--- Cryptography/TupperCipher.m
+++ Cryptography/TupperCipher.m
@@ -10,16 +10,21 @@
 
 (* ::Subsection:: *)
 (*Main Functions*)
+
+
 TupperCipher[text_String] := TupperCipher[text, 17];
-TupperCipher[text_String, p_?PrimeQ] := TupperCipher[Rasterize[Text@text, ImageSize -> {Automatic, p}], p];
+TupperCipher[text_String, p_?PrimeQ] := TupperCipher[
+	Rasterize[Text@text, ImageSize -> {Automatic, p}],
+	p
+];
 TupperCipher[img_Image] := TupperCipher[img, 17];
 TupperCipher[img_Image, p_?PrimeQ] := Block[
-	{i, m, r},
-	i = ImageResize[img, {Automatic, p}];
+	{i, m},
+	i = Binarize@ImageResize[img, {Automatic, p}];
 	m = Map[Boole[Max@# < 1]&, ImageData[i], {2}];
-	r = p * FromDigits[Flatten[Reverse@Transpose[m]], 2]
-]
+	p * FromDigits[Flatten[Reverse@Transpose[m]], 2]
+];
+
 
 (* ::Subsection:: *)
 (*Auxiliary Functions*)
-
--- Cryptography/TupperDecipher.m
+++ Cryptography/TupperDecipher.m
@@ -10,20 +10,37 @@
 
 (* ::Subsection:: *)
 (*Main Functions*)
+
+
 Options[TupperDecipher] = {
-	"Shift" -> Automatic
+	"Shift" -> Automatic,
+	Mesh -> True,
+	Frame -> True,
+	FrameStyle -> Opacity[0],
+	FrameTicksStyle -> Opacity[1],
+	FrameTicks -> All
 };
 TupperDecipher[n_, o : OptionsPattern[]] := TupperDecipher[n, 17, o];
-TupperDecipher[n_, p_?PrimeQ, o : OptionsPattern[]] := Module[
-	{bin, t},
-	t = Max[Ceiling@OptionValue["Shift"], 1];
-	bin = IntegerDigits[n / p, 2, p * Ceiling[IntegerLength[n / p, 2] / p]];
+TupperDecipher[n_, p_?PrimeQ, o : OptionsPattern[]] := Block[
+	{s, ws, pts},
+	If[!IntegerQ[n / p], Message[Decrypt::failed];Return[]];
+	s = If[
+		Internal`PositiveIntegerQ@OptionValue["Shift"],
+		OptionValue["Shift"],
+		pts = IntegerDigits[n / p, 2];
+		ws = First@FirstPosition[Reverse[Tr /@ Partition[pts, p]], _?(# != 0&)] - 1;
+		Ceiling[Length@pts / p] + ws
+	];
 	ArrayPlot[
-		Reverse /@ Transpose@Partition[bin, p]
+		Reverse /@ Transpose@Partition[IntegerDigits[n / p, 2, p * s], p],
+		Mesh -> OptionValue[Mesh],
+		Frame -> OptionValue[Frame],
+		FrameStyle -> OptionValue[FrameStyle],
+		FrameTicksStyle -> OptionValue[FrameTicksStyle],
+		FrameTicks -> OptionValue[FrameTicks]
 	]
 ]
 
+
 (* ::Subsection:: *)
 (*Auxiliary Functions*)
-
-
