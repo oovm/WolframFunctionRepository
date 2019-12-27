5ac04834917a4b80086ddd3048209324a066b800
Aster <galaster@foxmail.com>
2019-12-27 22:07:03 +0800

    🌿 Rewrite with case form

--- WarpDriveSpeed/WarpDriveSpeed.m
+++ WarpDriveSpeed/WarpDriveSpeed.m
@@ -10,17 +10,23 @@
 
 (* ::Subsection:: *)
 (*Main Functions*)
+
+
 WarpDriveSpeed[w_] := Power[w, (a[w] * b[w] * c[w] + 1) * 10 / 3];
+WarpDriveSpeed[w_] := 0 /; w <= 0;
+WarpDriveSpeed[w_] := Infinity /; w >= 10;
+
 
 (* ::Subsection:: *)
 (*Auxiliary Functions*)
 
-a[w_] := If[w < 7, 0, 0.20467Exp[-0.0058Log[100000 - 10000w]^5]];
-b[w_] := Block[
-	{s = Log[8 / (100 - 10 w)]},
-	If[w < 5 || w > 9.8, Return[1]];
-	1 + Exp[-49.369s^4](2 Cos[10 Pi s] - 1) / 3
-];
-c[w_] := 1 + (1.88269 / Pi) (Pi / 2 - ArcTan[10^w Log[20000 - 2000 w]]);
+
+a[w_] := 0.20467Exp[-0.0058Log[100000 - 10000w]^5];
+a[w_] := 0 /; w < 7;
 
 
+b[w_] := 1 /; w < 5 || w > 9.8;
+b[w_] := With[{s = Log[8 / (100 - 10 w)]}, 1 + Exp[-49.369s^4](2 Cos[10 Pi s] - 1) / 3];
+
+
+c[w_] := 1 + (Pi / 2 - ArcTan[10^w Log[20000 - 2000 w]])(1.88269 / Pi);
