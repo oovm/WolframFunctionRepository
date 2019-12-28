9d59c4bd05c26b45ddf874c75e5b4a799153b09a
Aster <galaster@foxmail.com>
2019-12-28 14:58:56 +0800

    🌿 Rewrite with piecewise

--- WarpDriveSpeed/WarpDriveSpeed.m
+++ WarpDriveSpeed/WarpDriveSpeed.m
@@ -12,21 +12,37 @@
 (*Main Functions*)
 
 
-WarpDriveSpeed[w_] := Power[w, (a[w] * b[w] * c[w] + 1) * 10 / 3];
-WarpDriveSpeed[w_] := 0 /; w <= 0;
-WarpDriveSpeed[w_] := Infinity /; w >= 10;
+WarpDriveSpeed["Expression"] := WarpDriveSpeed["Expression"] = Function[w,
+	Evaluate@Block[
+		{l = Log[10 - w], a, b, c, d},
+		a = Exp[-29 (Log[10000] + l)^5 / 5000];
+		b = 388269 Pi - 376538 ArcTan[10^w (Log[2000] + l)] ;
+		c = Exp[-49369 (Log[4 / 5] + l)^4 / 1000];
+		d = -1 + 2 Cos[10 Pi (Log[4 / 5] + l)];
+		Power[w, 10 / 3 + 20467 * a * b * (3 + c * d) / 18000000000 / Pi]
+	]
+];
+WarpDriveSpeed[w_] := Piecewise[{
+	{Power[w, (a[w] * b[w] * c[w] + 1) * 10 / 3], 0 < w < 10},
+	{0, w <= 0},
+	{Infinity, w >= 10}
+}];
 
 
 (* ::Subsection:: *)
 (*Auxiliary Functions*)
 
 
-a[w_] := 0.20467Exp[-0.0058Log[100000 - 10000w]^5];
-a[w_] := 0 /; w < 7;
+a[w_] := Piecewise[{
+	{20467 Exp[-29Log[100000 - 10000w]^5 / 5000] / 100000, w > 8},
+	{0, w <= 8}
+}];
 
 
-b[w_] := 1 /; w < 5 || w > 9.8;
-b[w_] := With[{s = Log[8 / (100 - 10 w)]}, 1 + Exp[-49.369s^4](2 Cos[10 Pi s] - 1) / 3];
+b[w_] := Piecewise[{
+	{With[{s = Log[8 / (100 - 10 w)]}, 1 + Exp[-49369s^4 / 1000](2 Cos[10 Pi s] - 1) / 3], 5 <= w <= 49 / 5},
+	{1, w < 5 || w > 49 / 5}
+}];
 
 
-c[w_] := 1 + (Pi / 2 - ArcTan[10^w Log[20000 - 2000 w]])(1.88269 / Pi);
+c[w_] := 1 + (Pi / 2 - ArcTan[10^w Log[20000 - 2000 w]]) * (188269 / 100000 / Pi)
